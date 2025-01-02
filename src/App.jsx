9efa4687bb8c51454226cd6b2c2a399d162b3e51
Pem Tsering Gurung <pemgurung541@gmail.com>
2025-01-02 17:13:38 -0500

    01/02/2025 Refreshed about props

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import {CORE_CONCEPTS } from "./data.js"
+import Header from './components/Header/Header.jsx'
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+
+
+function App() {
+  function handleClick(){
+    console.log("Hello World! - selected")
+}
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id = "core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+              <TabButton onSelect={handleClick}>Components</TabButton>
+              <TabButton onSelect={handleClick}>JSX</TabButton>
+              <TabButton onSelect={handleClick}>Props</TabButton>
+              <TabButton onSelect={handleClick}>State</TabButton>
+          </menu>
+
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
